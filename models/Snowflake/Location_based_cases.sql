c82a3d9cdbf9fff266143a9b2b449eb80d45552b
Sathyanarayanan RV <48621734+sathyarviswanath@users.noreply.github.com>
2022-01-09 14:42:06 +0530

    query3

--- models/Snowflake/Location_based_cases.sql
+++ models/Snowflake/Location_based_cases.sql
@@ -1,4 +1,6 @@
-select location, sum(new_cases) as total_cases
+select location, sum(new_active_cases) as total_new_active_cases, sum(new_cases) as total_new_cases, sum(total_cases) as overall_cases,
+sum(total_deaths) as total_deaths, sum(new_deaths) as total_new_deaths, sum(new_recovered) as total_new_cases_recovered,
+sum(total_recovered) as overall_recovered
 from
 {{ source('COVID_19_INDONESIA_SATHYANARAYANAN_CV','COVID_19_INDONESIA_SATHYANARAYANAN_CV')}}
 group by 1;
