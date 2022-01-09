85db71d57e5e1900e757cad872e3c09b40c34253
Sathyanarayanan RV <48621734+sathyarviswanath@users.noreply.github.com>
2022-01-09 14:50:32 +0530

    update

--- models/Snowflake/Location_based_active_cases.sql
+++ models/Snowflake/Location_based_active_cases.sql
@@ -1,4 +1,4 @@
 select location, sum(new_active_cases) as total_active_cases
 from
  {{ source('COVID_19_INDONESIA_SATHYANARAYANAN_CV','COVID_19_INDONESIA_SATHYANARAYANAN_CV')}}
-group by 1;
+group by 1
