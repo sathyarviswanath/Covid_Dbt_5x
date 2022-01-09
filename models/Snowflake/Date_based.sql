baf4aee48313554c890874f3001885e126d9c3c1
Sathyanarayanan RV <48621734+sathyarviswanath@users.noreply.github.com>
2022-01-09 15:07:26 +0530

    c5

---
+++ models/Snowflake/Date_based.sql
@@ -0,0 +1,11 @@
+select * , round(overall_recovered/overall_cases,2)*100 as rate_of_recovered 
+from (
+  select  date ,
+ sum(new_active_cases) as total_new_active_cases, sum(new_cases) as total_new_cases, sum(total_cases) as overall_cases,
+sum(total_deaths) as total_deaths, sum(new_deaths) as total_new_deaths, sum(new_recovered) as total_new_cases_recovered,
+sum(total_recovered) as overall_recovered
+  from
+{{ source('COVID_19_INDONESIA_SATHYANARAYANAN_CV','COVID_19_INDONESIA_SATHYANARAYANAN_CV')}}
+   group by 1
+  )
+  order by 1 desc, 9 desc 
